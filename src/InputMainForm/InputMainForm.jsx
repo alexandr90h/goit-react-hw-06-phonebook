0fba81badc5d51eaeaff66f5edc3696ca9dedfc9
Olexandr <63474923+alexandr90h@users.noreply.github.com>
2021-01-12 16:21:09 +0200

    12.01.2021-2

--- src/InputMainForm/InputMainForm.jsx
+++ src/InputMainForm/InputMainForm.jsx
@@ -2,12 +2,20 @@ import PropTypes from 'prop-types';
 import styles from './inputMainForm.module.scss';
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 export default function InputMainForm(props) {
+        const schema = yup.object().shape({
+        name: yup.string().required().min(3),
+        number:yup.number().required().min(5),
+    })
+
+    const { register,handleSubmit,reset, errors } = useForm({resolver:yupResolver(schema)});
     const [name, setName] = useState('');
     const [number, setNumber] = useState('');
     const [id, setId] = useState(uuidv4());
-    
     const hendleInputChanga = e => {
         switch (e.target.name) {
             case 'name': setName(e.target.value)
@@ -17,26 +25,29 @@ export default function InputMainForm(props) {
             default:
                 break;
         }
+        console.log(errors);
     }
-    const hendleOnSubmit = e => {
-        e.preventDefault();
+    const hendleOnSubmite = e => {
+        // e.preventDefault();
        props.onSubHand({ name, number, id });
-        reset();
+        // reset();
     }
-    const reset = () => {
-        setName('');
-        setNumber('');
-        setId(uuidv4());
-    }
+    // const reset = () => {
+    //     setName('');
+    //     setNumber('');
+    //     setId(uuidv4());
+    // }
      return (
             <div>
-            <form onSubmit={hendleOnSubmit} className={styles.mainForm}>
+            <form onSubmit={handleSubmit(hendleOnSubmite)} className={styles.mainForm}>
                 <label>Name 
-            <input type="text" name="name" autoComplete="off" value={name} onChange={hendleInputChanga}/>
-                </label>
+            <input type="text" name="name" autoComplete="off" value={name} onChange={hendleInputChanga} ref={register} />
+                 </label>
+                 {errors.name && <p>{errors.name.message}</p>}
                 <label>Number
-                     <input type="text" name="number" autoComplete="off" value={number} onChange={hendleInputChanga}/>
-                </label>
+                     <input type="text" name="number" autoComplete="off" value={number} onChange={hendleInputChanga} ref={register}/>
+                 </label>
+                 {errors.number && <p>{ errors.number.message}</p>}
                 <button type="submit">Add contact</button>
         </form>
             </div>
