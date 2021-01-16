ba0118c71540026345f5ed1a1d52925e2051d76b
Alexandr <63474923+alexandr90h@users.noreply.github.com>
2021-01-16 23:47:58 +0200

    16.01.2021-3

--- src/InputFind/InputFind.jsx
+++ src/InputFind/InputFind.jsx
@@ -1,17 +1,20 @@
 import styles from './inputFind.module.scss';
 import PropTypes from 'prop-types';
-import { useDispatch } from "react-redux";
+import {useSelector, useDispatch } from "react-redux";
 import { FiX } from "react-icons/fi";
 import contactsAction from "../redux/action.js";
 
 
 export default function InputFind() {
-        const dispatch = useDispatch();
+    const dispatch = useDispatch();
+    const filterName=useSelector(state=>state.filter)
     const resetInputFind = () => {
         dispatch(contactsAction.filterChange(''));
     }
     const hendlInputFilter = e => {
-        dispatch(contactsAction.filterChange(e.currentTarget.value))
+        dispatch(contactsAction.filterChange(e.currentTarget.value));
+        console.log(filterName);
+        filterName ? document.querySelector("#btn-reset").style.color = "red" : document.querySelector("#btn-reset").style.color = "rgb(192, 192, 192)";
     }
     return (
             <div className={styles.inputContainer}>
@@ -20,7 +23,7 @@ export default function InputFind() {
                 <input type="text"
                     autoComplete="off"
                     onChange={hendlInputFilter} />
-                <button type="reset" onClick={resetInputFind}><FiX /></button>
+                <button type="reset" onClick={resetInputFind} id="btn-reset"><FiX /></button>
             </form>
             </div>
         )
