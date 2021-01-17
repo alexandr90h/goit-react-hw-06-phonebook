a633fb7fe55c83c76c3623a1c7973539220be81c
Alexandr <63474923+alexandr90h@users.noreply.github.com>
2021-01-17 20:20:34 +0200

    end

--- src/InputFind/InputFind.jsx
+++ src/InputFind/InputFind.jsx
@@ -1,20 +1,19 @@
 import styles from './inputFind.module.scss';
 import PropTypes from 'prop-types';
-import {useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { FiX } from "react-icons/fi";
 import contactsAction from "../redux/action.js";
 
 
 export default function InputFind() {
     const dispatch = useDispatch();
-    const filterName=useSelector(state=>state.filter)
     const resetInputFind = () => {
         dispatch(contactsAction.filterChange(''));
+        document.querySelector("#btn-reset").style.color = "rgb(192, 192, 192)";
     }
     const hendlInputFilter = e => {
         dispatch(contactsAction.filterChange(e.currentTarget.value));
-        console.log(filterName);
-        filterName ? document.querySelector("#btn-reset").style.color = "red" : document.querySelector("#btn-reset").style.color = "rgb(192, 192, 192)";
+        e.currentTarget.value ? document.querySelector("#btn-reset").style.color = "red" : document.querySelector("#btn-reset").style.color = "rgb(192, 192, 192)";
     }
     return (
             <div className={styles.inputContainer}>
